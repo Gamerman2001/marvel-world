2a89b46b11169a0fe53bb05913755283048a76ef
Gamerman2001 <Tro12185@gmail.com>
2020-01-16 02:04:39 -0500

    styling page, added form, added boostrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="jumbotron">
+        <div className="container">
+          <h1 className="header-main-title"> Marvel Search</h1>
+          <form action="" id="connectionForm">
+            <div className="form-group">
+              <input
+                required
+                type="text"
+                name="name"
+                id="name"
+                className="form-control character-search-box"
+                placeholder="Enter a Character"
+              />
+            </div>
+            <input
+              type="submit"
+              value="Search!"
+              className="btn btn-danger mb-2 float-right sserach-character-button"
+            />
+          </form>
+        </div>
+      </div>
+      <div className="continer" id="contentContainer">
+        <div
+          className="d-flex align-items-center"
+          id="characterSpinnerSection"
+        ></div>
+        <div
+          className="d-flex align-items-center"
+          id="comicsSpinnerSection"
+        ></div>
+      </div>
+
+      <section id="characterSection"></section>
+
+      <section id="comicSection"></section>
     </div>
   );
 }
